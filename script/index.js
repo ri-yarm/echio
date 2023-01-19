466fdb6f117478fedc85ce507a2259f57a9dbb98
Rinat Yarmuhametov <thogusus@gmail.com>
2023-01-19 16:05:14 +0500

    slider well done

---
+++ script/index.js
@@ -0,0 +1,30 @@
+(function () {
+  //функция слайдера
+  function slideEnable() {
+    const slider = document.querySelector('.slider'),
+        slide = document.querySelector('.slide'),
+        slides = Array.from(document.querySelectorAll('.slide')),
+        btnSlideLeft = document.querySelector('.slide_left'),
+        btnSlideRight = document.querySelector('.slide_right');
+
+  let slideIterator = 0
+  const stepSize = slides[0].clientWidth - 90
+
+  function slideRight() {
+    slideIterator >= slides.length - 1 ? slideIterator = -1 : null
+    slideIterator++
+    slider.style.transform = `translateX(${-stepSize * slideIterator}px)`
+  }
+
+  function slideLeft() {
+    slideIterator <= 0 ? slideIterator = slides.length : null
+    slideIterator--
+    slider.style.transform = `translateX(${-stepSize * slideIterator}px)`
+  }
+
+  btnSlideRight.addEventListener('click', () => slideRight());
+  btnSlideLeft.addEventListener('click', () => slideLeft())
+  }
+  // включение слайдера
+  slideEnable()
+})();
